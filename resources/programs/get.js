d9cdf38a0ce7d54fdb3ee9e62b6fd8a1d7109503
David Fullmer <david.fullmer@gtri.gatech.edu>
2016-08-21 13:04:22 -0400

    1. updated program drop down in programs.html 2. update db to support nameAndDesc from programs

--- resources/programs/get.js
+++ resources/programs/get.js
@@ -2,6 +2,7 @@ if(me && (me.id === this.owner || me.type === 'admin')) {
   dpd.students.get({program: this.id}, function(students) {
     this.students = students;
     this.numEnrolled = students.length;
+    this.nameAndDesc = this.name + '   ['+ this.description  +']';
   });
   dpd.users.get({id: this.owner}, function(owner){
     this.ownerName = owner.username;
