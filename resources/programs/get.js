37ef874a6ec1b5f38c12ee86258c9a858a3230fa
David Fullmer <david.fullmer@gtri.gatech.edu>
2016-08-21 15:21:47 -0400

    1. gave teachers role permission to access all students and programs (read only) 2. finished class.html

--- resources/programs/get.js
+++ resources/programs/get.js
@@ -1,12 +1,14 @@
-if(me && (me.id === this.owner || me.type === 'admin')) {
-  dpd.students.get({program: this.id}, function(students) {
-    this.students = students;
-    this.numEnrolled = students.length;
-    this.nameAndDesc = this.name + '   ['+ this.description  +']';
-  });
-  dpd.users.get({id: this.owner}, function(owner){
-    this.ownerName = owner.username;
-  });
+if(me && (me.id === this.owner || me.type === 'admin' || me.type === 'teacher')) {
+    if(!query.raw){
+        dpd.students.get({program: this.id, raw: true}, function(students) {
+            this.students = students;
+            this.numEnrolled = students.length;
+            this.nameAndDesc = this.name + '   ['+ this.description  +']';
+        });
+        dpd.users.get({id: this.owner}, function(owner){
+            this.ownerName = owner.username;
+        }); 
+    }
  }else{
      cancel();
  }
--- resources/students/get.js
+++ resources/students/get.js
@@ -1,5 +1,22 @@
-if(me && (me.id === this.owner || me.type === 'admin')) {
- 
- }else{
+if(me && (me.id === this.owner || me.type === 'admin' || me.type === 'teacher')) {
+    if(!query.raw){
+        dpd.programs.get({id: this.program}, function(program) {
+            this.programName = program.name;
+            this.programNameAndDesc = program.name + '   ['+ program.description  +']';
+            this.monday = program.monday
+            this.tuesday = program.tuesday
+            this.wednesday = program.wednesday
+            this.thursday = program.thursday
+            this.friday = program.friday
+            this.saturday = program.saturday
+            this.sunday = program.sunday
+        });
+        dpd.teachers.get({id: this.teacher}, function(teacher) {
+            this.teacherName = teacher.lastName;
+            this.teacherNameAndGrade = teacher.lastName + '   [ Grade: ' + teacher.grade + ' ]';
+        });
+    }
+
+}else{
      cancel()
- }
+}
